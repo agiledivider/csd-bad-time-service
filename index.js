580aadf287a1d43f1445c97fe6470dbed30c001e
Falk Kühnel <falk@kuehnel.eu>
2023-11-16 11:56:12 +0100

    time service now can be good or bad

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ const port = 80
 const process = require('process')
 
 var counter = 0;
+var amIBad = false;
 app.get('/', (req, res) => {
   counter++
-  if (counter % 3 === 0) {
+  if (amIBad && counter % 3 === 0) {
     res.status(500).end()
   } else {
     res.send(Date.now().toString())
   }
+})
+
+app.get('/bebad/', (req, res) => {
+  amIBad = true
+  res.send('I am bad now')
+})
+
+app.get('/begood/', (req, res) => {
+  amIBad = false
+  res.send('I am good now')
+})
+
+app.get('/status/', (req, res) => {
+  if (amIBad) {
+    res.send('I am bad')
+  } else {
+    res.send('I am good')
+  }
+})
+
+app.get('/counter/', (req, res) => {
+  res.send('count: ' + counter)
 })
 
 process.on('SIGINT', () => {
